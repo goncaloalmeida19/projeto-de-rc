960366d99dcb75340f91a8cf63f522caf9db5116
Gonçalo Almeida <gamertota4@gmail.com>
2022-05-24 19:32:53 +0100

    Subscribe markets

--- operations_terminal.c
+++ operations_terminal.c
@@ -18,6 +18,7 @@
 #define MSG_LEN WORD_LEN*20
 
 char username[WORD_LEN], market1[WORD_LEN], market2[WORD_LEN];
+int n_markets, fd;
 
 int login(char buffer[MSG_LEN], char msg[MSG_LEN]){
     int option = 0, login_error;
@@ -28,6 +29,7 @@ int login(char buffer[MSG_LEN], char msg[MSG_LEN]){
         printf("Name Password: ");
         scanf("%s %s", username, password);
         sprintf(msg, "login %s %s", username, password);
+        write(fd, msg, strlen(msg) + 1);
         return 0;
     }
     else if(sscanf(buffer, "login %d", &login_error) == 1) {
@@ -38,26 +40,29 @@ int login(char buffer[MSG_LEN], char msg[MSG_LEN]){
         }
         printf("1 - Try again\n2 - Close console\nOption: \n");
         scanf("%d", &option);
-        if (option == 1) {
-            printf("Name Password: ");
-            scanf("%s %s", username, password);
-            sprintf(msg, "login %s %s", username, password);
-            return 0;
-        } else if (option == 2){
-            printf("Console closed.\n");
-            return -1;
-        }
-        else {
-            printf("Invalid command.\n");
-            return 2;
+        switch (option){
+            case 1:
+                printf("Name Password: ");
+                scanf("%s %s", username, password);
+                sprintf(msg, "login %s %s", username, password);
+                write(fd, msg, strlen(msg) + 1);
+                return 0;
+            case 2:
+                printf("Console closed.\n");
+                return -1;
+            default:
+                printf("Invalid command.\n");
+                return 2;
         }
     }
     else if(sscanf(buffer, "login %s", market1) == 1) {
         printf("Login successful! Client can access to the market %s.\n", market1);
+        n_markets = 1;
         return 1;
     }
     else if(sscanf(buffer, "login %s %s", market1, market2) == 1) {
         printf("Login successful! Client can access to the markets %s and %s.\n", market1, market2);
+        n_markets = 2;
         return 1;
     }
     else if(strncmp(buffer, "login", 5) == 0) {
@@ -70,14 +75,71 @@ int login(char buffer[MSG_LEN], char msg[MSG_LEN]){
     }
 }
 
+void subscribe_markets(){
+    char buffer[MSG_LEN], msg[MSG_LEN], market[MSG_LEN];
+    int option = 0, nread, subscribe_error;
+    printf("Market name: ");
+    scanf("%s", market);
+
+    sprintf(msg, "subscribe %s", market);
+    write(fd, msg, strlen(msg) + 1);
+
+    nread = read(fd, buffer, MSG_LEN-1);
+    if(nread <= 0){
+        printf("Server closed.\n");
+    } else{
+        if(sscanf(buffer, "subscribe 0 %s", market1) == 1) {
+            printf("Login successful! Client can access to the market %s.\n", market1);
+        }
+    }
+}
+
 int menu(){
-    printf("Menu:\n"
-           "1 - Subscribe market\n"
-           "2 - Buy/Sell stocks\n"
-           "3 - Turn off stocks update feed\n"
-           "4 - Wallet information\n"
-           "5 - Close session\n"
-           "Option: ");
+    int option = 0, b_s_option = 0, ver = 1;
+    while(ver == 1) {
+        printf("Menu:\n"
+               "1 - Subscribe market\n"
+               "2 - Buy/Sell shares\n"
+               "3 - Turn on/off stocks update feed\n"
+               "4 - Wallet information\n"
+               "5 - Close session\n"
+               "Option: ");
+        scanf("%d", &option);
+        switch (option) {
+            case 1:
+                subscribe_markets();
+                break;
+            case 2:
+                printf("Menu:\n"
+                       "1 - Buy share\n"
+                       "2 - Sell share\n"
+                       "3 - Go back\n"
+                       "Option: ");
+                scanf("%d", &b_s_option);
+                if (b_s_option == 1)
+                    buy_share();
+                else if (b_s_option == 2)
+                    sell_share();
+                else if (b_s_option == 3)
+                    continue;
+                else
+                    printf("Invalid command.\n");
+                break;
+            case 3:
+                turn_on_off_stock_update_feed();
+                break;
+            case 4:
+                get_wallet_info();
+                break;
+            case 5:
+                ver = -1;
+                break;
+            default:
+                printf("Invalid command.\n");
+                break;
+        }
+    }
+    if (ver == -1) return -1;
     return 0;
 }
 
@@ -85,7 +147,7 @@ int main(int argc, char *argv[]){
     struct hostent *hostPtr;
     struct sockaddr_in addr;
     char server_addr[WORD_LEN], buffer[MSG_LEN], msg[MSG_LEN];
-    int fd, nread = 0, login_return = 0;
+    int nread = 0, login_return = 0;
 
     if(argc != 3){
         printf("Wrong number of arguments\n");
@@ -118,20 +180,16 @@ int main(int argc, char *argv[]){
         if (login_return != 1)
             login_return = login(buffer, msg);
         else
-            menu();
+            if (menu() == -1)
+                login_return = 0;
 
         if(login_return == -1)
             break;
         else if(login_return == 0 || login_return == 2) {
-            write(fd, msg, strlen(msg) + 1);
             continue;
         } else if(login_return == 1){
             menu();
         }
-
-        // Sent input to the server
-        write(fd, msg, strlen(msg)+1);
-
     }while(1);
 
     close(fd);
